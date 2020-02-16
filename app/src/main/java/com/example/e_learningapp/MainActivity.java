f50fb877db865098c035233db2eeb56df1c96a1e
FlRE-DEATH <48518316+FlRE-DEATH@users.noreply.github.com>
2020-02-16 20:05:10 +0200

    added more comments

--- app/src/main/java/com/example/e_learningapp/MainActivity.java
+++ app/src/main/java/com/example/e_learningapp/MainActivity.java
@@ -31,9 +31,13 @@ public class MainActivity<a> extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {   //asta e clasa in care runnuim cam tot codul. gotta figure out how to make more classes and stuff like that
         //so that I don't write all the code here in the main.
-        //DE FAPT AR TREBUI SA SCRII IN FUNCTIA onResume !!!
+        //DE FAPT AR TREBUI SA SCRII IN FUNCTIA onResume !!! e un fel de constructor.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //aici initializez variabile and stuff. poate as putea face asta in alta clasa, iar aici sa dau override doar la onresume.
+        //la final cum fac pasii de serializare si persistenta? as in, daca nu vreau ca de fiecare data cand pornesc aplicatia sa am nevoie de initializare
+        //pentru ca faza asta ar fi foarte time consuming pentru aplicatii mari.
+        //in manifest trebe sa fie declarate toate activitatile.
 
         EmailText =  findViewById(R.id.email_textInput);
         PasswordText =  findViewById(R.id.password_textInput);
@@ -76,4 +80,11 @@ public class MainActivity<a> extends AppCompatActivity
     //la randul lor, clasele astea la onclick ma vor conduce la niste layouturi noi, unde ar trebui sa fie lectiile.
     //BUT FIRST let's experiment with adding new files to make all the declarations there to have a cleaner main.
     // set on click listener ar fi bine sa fie si el in alte fisiere oare? daca da, cum implementez asta/cum fac functiile respective?
+
+
+
+
+
 }
+
+
