c5030431652c966f4c2fcfd36f6f5f5220afaca9
FlRE-DEATH <48518316+FlRE-DEATH@users.noreply.github.com>
2020-02-16 17:41:39 +0200

    Added comments and a basic login verification if.

--- app/src/main/java/com/example/e_learningapp/MainActivity.java
+++ app/src/main/java/com/example/e_learningapp/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity<a> extends AppCompatActivity
     String email;
     String password;
 
+    String usermail="a";
+    String userpass="b";
+
     EditText EmailText;
     EditText PasswordText;
 
@@ -25,34 +28,50 @@ public class MainActivity<a> extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
-    {
+    {   //asta e clasa in care runnuim cam tot codul. gotta figure out how to make more classes and stuff like that
+        //so that I don't write all the code here in the main.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EmailText = (EditText) findViewById(R.id.email_textInput);
-        PasswordText = (EditText) findViewById(R.id.password_textInput);
+        EmailText =  findViewById(R.id.email_textInput);
+        PasswordText =  findViewById(R.id.password_textInput);
 
-        LogInButton = (Button) findViewById(R.id.login_button);
+        LogInButton =  findViewById(R.id.login_button);
+        //aici fac tranzitia dintre layoutul de login screen si cel de lessons. if-ul e pus la caterinca.
+        // Am facut un LogInButton.setOnClickListener si asa merge.
+        {
 
-        LogInButton.setOnClickListener
-                (new View.OnClickListener()
-                 {
-             @Override
-             public void onClick(View V)
-             {
-                email = EmailText.getText().toString();
-                password = PasswordText.getText().toString();
-                showToast(email);
-                showToast(password);
-                 setContentView(R.layout.lessons_ui);
-             }
-
-                 }
-                );
+            LogInButton.setOnClickListener
+                    (new View.OnClickListener() {
+                         @Override
+                         public void onClick(View V) {
+                             email = EmailText.getText().toString();
+                             password = PasswordText.getText().toString();
 
 
-        //seteaza care layout il va afisa run-ul chestiei...?
-    }
+                             if (email.equals(usermail)&&password.equals(userpass))
+                             {
+                                 showToast("Welcome");
+                                 setContentView(R.layout.lessons_ui);
+                             }
+                             else
+                             {
+                                 showToast("Login Failed");
+                             }
 
-    //hai sa incerc un if care ma muta din main activity in lessons ui dupa ce apas pe log in
+                         }
+
+                     }
+                    );
+
+
+            //seteaza care layout il va afisa run-ul chestiei...?
+
+        }
+    } //end OnCreate method
+
+    //acum in layoutul de lessons trebuie sa declar clasa fiecarei materii si sa o fac clickable
+    //la randul lor, clasele astea la onclick ma vor conduce la niste layouturi noi, unde ar trebui sa fie lectiile.
+    //BUT FIRST let's experiment with adding new files to make all the declarations there to have a cleaner main.
+    // set on click listener ar fi bine sa fie si el in alte fisiere oare? daca da, cum implementez asta/cum fac functiile respective?
 }
